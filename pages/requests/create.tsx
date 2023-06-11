4caa32896472ab5e1b94a8448033f2733896e15c
DanilovG103 <danilov2706@gmail.com>
2023-06-11 17:19:59 +0400

    feat: add redirect after create request

--- pages/requests/create.tsx
+++ pages/requests/create.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useMemo } from 'react'
 import { Formik, FormikConfig } from 'formik'
+import { useRouter } from 'next/router'
 
 import { CreateRequestForm, Layout, Text } from 'src/components'
 import { useUserContext } from 'src/context'
@@ -9,11 +10,13 @@ import {
   useRequestMutation,
 } from 'src/generated'
 import type { CreateRequestValues } from 'src/types'
+import { Route } from 'src/utils'
 
 type Submit = FormikConfig<CreateRequestValues>['onSubmit']
 
 export default function CreateRequestPage() {
   const { user } = useUserContext()
+  const { push } = useRouter()
   const [createRequest] = useRequestMutation()
   const { data } = useBasketQuery({
     variables: {
@@ -38,23 +41,27 @@ export default function CreateRequestPage() {
   const handleSubmit: Submit = useCallback(
     async (values) => {
       if (!user) return
-      await createRequest({
-        variables: {
-          data: {
-            ...values,
-            goods: {
-              connect: data?.basket?.goods?.map((item) => ({ id: item.id })),
-            },
-            user: {
-              connect: {
-                id: user.id,
+
+      try {
+        await createRequest({
+          variables: {
+            data: {
+              ...values,
+              goods: {
+                connect: data?.basket?.goods?.map((item) => ({ id: item.id })),
+              },
+              user: {
+                connect: {
+                  id: user.id,
+                },
               },
             },
           },
-        },
-      })
+        })
+        push(Route.me)
+      } catch {}
     },
-    [createRequest, data?.basket?.goods, user],
+    [createRequest, data?.basket?.goods, push, user],
   )
 
   return (
